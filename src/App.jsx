66f8b051ae01f0b85ab424cbe45dbafdf1968bde
DehLuizSantos <65002993+DehLuizSantos@users.noreply.github.com>
2021-09-02 19:42:30 -0300

    build(initial):structure

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+/* import { Home } from "./pages/Home";
+import { NewRoom } from "./pages/NewRoom";
+
+import { BrowserRouter, Route } from "react-router-dom";
+
+import { AuthContextProvider } from "./context/AuthContext"; */
+
+function App() {
+  return (   
+
+    <div>
+      <h2>Exemplo</h2>
+    </div>
+  )
+}
+export default App;
